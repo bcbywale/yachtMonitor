17b56c872bab10c4f406cb057a4b56c42fabc365
bcbywale <bryanbywalec@gmail.com>
2014-12-05 17:51:00 -0600

    Updated xml parse loop

--- yachtMonitor/main.py
+++ yachtMonitor/main.py
@@ -6,6 +6,8 @@ import os
 configDir = os.path.dirname(__file__)
 configFilename = os.path.join(configDir, 'config.xml')
 configTree = ET.parse(configFilename)
+configRoot = configTree.getroot()
+
 
 def calculate(*args):
     try:
@@ -14,47 +16,61 @@ def calculate(*args):
     except ValueError:
         pass
 
+def loadConfig():
+    global configTree, configRoot
+    configTree = ET.parse(configFilename)
+    configRoot = configTree.getroot()
+
 def configure():
-    print("Configure")
+
     window = tk.Toplevel()
     label = tk.Label(window,text="Configuration")
     label.pack(side="top", fill="both", padx=10, pady=10)
+    #build interface to adjust configuration of connection to arduino
 
-def loadConfig():
+    #OK button should reload config on the way out
+    loadConfig()
 
-    pass
+    #DEBUG
+    for child in configRoot:
+        print(child.tag, child.attrib)
 
-root = tk.Tk()
+#Before we do anything, let's load the configuration
+loadConfig()
 
-root.title("Yacht Monitor V0.1")
+if __name__ == "__main__":
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
+    root = tk.Tk()
 
-menubar = tk.Menu(root)
-menubar.add_command(label="Configure", command=configure)
-menubar.add_command(label="Quit!", command=root.quit)
-root.config(menu=menubar)
+    root.title("Yacht Monitor V0.1")
+
+    mainframe = ttk.Frame(root, padding="3 3 12 12")
+    mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+    mainframe.columnconfigure(0, weight=1)
+    mainframe.rowconfigure(0, weight=1)
+
+    menubar = tk.Menu(root)
+    menubar.add_command(label="Configure", command=configure)
+    menubar.add_command(label="Quit!", command=root.quit)
+    root.config(menu=menubar)
 
 
 
-feet = tk.StringVar()
-meters = tk.StringVar()
+    feet = tk.StringVar()
+    meters = tk.StringVar()
 
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
+    feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+    feet_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
 
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(tk.W, tk.E))
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=tk.W)
+    ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(tk.W, tk.E))
+    ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=tk.W)
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=tk.W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=tk.E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=tk.W)
+    ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=tk.W)
+    ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=tk.E)
+    ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=tk.W)
 
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+    for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
-feet_entry.focus()
+    feet_entry.focus()
 
-root.mainloop()
+    root.mainloop()
