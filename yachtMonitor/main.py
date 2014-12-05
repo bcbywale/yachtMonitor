a927c49ceb0759b9483557d109b0d52df5149948
bcbywale <bryanbywalec@gmail.com>
2014-12-05 17:17:18 -0600

    First Code Upload
    Opens windows, parses xml config

--- yachtMonitor/main.py
+++ yachtMonitor/main.py
@@ -1,8 +1,60 @@
-'''
-Created on 05 Dec,2014
+import tkinter as tk
+from tkinter import ttk
+import xml.etree.ElementTree as ET
+import os
 
-@author: bryan.bywalec
-'''
+configDir = os.path.dirname(__file__)
+configFilename = os.path.join(configDir, 'config.xml')
+configTree = ET.parse(configFilename)
 
-if __name__ == '__main__':
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+def configure():
+    print("Configure")
+    window = tk.Toplevel()
+    label = tk.Label(window,text="Configuration")
+    label.pack(side="top", fill="both", padx=10, pady=10)
+
+def loadConfig():
+
     pass
+
+root = tk.Tk()
+
+root.title("Yacht Monitor V0.1")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+menubar = tk.Menu(root)
+menubar.add_command(label="Configure", command=configure)
+menubar.add_command(label="Quit!", command=root.quit)
+root.config(menu=menubar)
+
+
+
+feet = tk.StringVar()
+meters = tk.StringVar()
+
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(tk.W, tk.E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=tk.W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=tk.W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=tk.E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=tk.W)
+
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+
+root.mainloop()
